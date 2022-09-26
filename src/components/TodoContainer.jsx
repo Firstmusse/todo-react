cc654da7e56820f1d779d3f46ef72d092075ec84
Aleksandr <mussefirst1@gmail.com>
2022-09-26 17:55:09 +0600

    add input state

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,15 +1,48 @@
-import './TodoContainer.css'
+import React, {useState} from 'react'
 
+import './TodoContainer.css' 
+import trash from '../icons/remove.png'
 
 function TodoContainer() {
+
+    const [change, setChange] = useState('')
+
+    const [inputValue, setInputValue] = useState([
+
+    ])
+    console.log(change);
+
+    
+
+
   return (
     <div className="todo">
             <div className="todo-container">
                     <div className="todo-content-wrapper">
-                        <div className="todo-input-block">
-                            <input className='todo-input' type="text" placeholder='Введите задачу' />
+                       <div className="todo-content">
+                       <div className="todo-input-block">
+                            <input
+                            value={change}
+                             onChange={(e)=> setChange(e.target.value)}
+                             className='todo-input' 
+                             type="text" 
+                             placeholder='Введите задачу' />
+                            
                         </div>
+                        <div className="todo-click-block">
+                        <div className="todo-link-block">
+                            <a href="#" className='todo-link'>Все(0)</a>
+                            <a href="#" className='todo-link'>Выполненые</a>
+                        </div>
+                        <div className="todo-trash-block">
+                            <div className="todo-trash">
+                                <img src={trash} alt="#" />
+                            </div>
+                        </div>
+                        </div>
+                       </div>
                     </div>
+
             </div>
     </div>
   )
